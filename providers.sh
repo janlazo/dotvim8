2ac504d72afa980faeff574062a422f0a485c8ed
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-01-02 19:36:22 -0500

    provider: ruby,node clients work in Windows

--- providers.sh
+++ providers.sh
@@ -8,7 +8,9 @@ pip2 install neovim
 pip3 install neovim
 
 # Ruby - https://github.com/alexgenco/neovim-ruby
+# Require 0.6.2+ for Windows
 gem install --conservative neovim
 
 # Node - https://github.com/neovim/node-client
-# npm install -g neovim
+# Require 3.5.2+ for Windows
+npm install -g neovim
