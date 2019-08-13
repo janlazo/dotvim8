876666eb8141e2a4e47bb5c129178d6cd706bf50
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-08-13 00:20:10 -0400

    ci: use nvm to test nodejs on Travis
    Suppress shellcheck SC1090 error.
    Run bash to avoid SC2240 error.

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 cd "$(dirname "$0")/.."
+
+NVM_DIR="$HOME/.nvm"
+test -f "$NVM_DIR/nvm.sh" && source "$NVM_DIR/nvm.sh" --no-use
+if command -v nvm >/dev/null 2>&1; then
+  nvm install lts/carbon
+  nvm use lts/carbon
+fi
 
 ./bin/install_deps.sh
--- .ci/script.sh
+++ .ci/script.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 cd "$(dirname "$0")/.."
 
 if command -v shellcheck > /dev/null 2>&1; then
-  shellcheck -e SC2046 $(git ls-files '*.sh')
+  shellcheck -e SC1090 -e SC2046 $(git ls-files '*.sh')
 fi
 
 VIM_ARGS="-V2 -Es --cmd 'version' --cmd 'set rtp=$PWD,\$VIMRUNTIME,$PWD/after'"
