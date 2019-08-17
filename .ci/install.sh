e46d6f34f409cfbd46708c48f5013df8e027f1c5
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-08-17 13:02:48 -0400

    ci: debug node,python,ruby versions

--- .ci/install.sh
+++ .ci/install.sh
@@ -10,4 +10,13 @@ source "$NVM_DIR/nvm.sh" --no-use
 nvm install lts/carbon
 nvm use lts/carbon
 
+echo node $(node --version)
+echo npm $(npm --version)
+python --version
+python -m pip --version
+python3 --version
+python3 -m pip --version
+ruby --version
+echo gem $(gem --version)
+
 ./bin/install_deps.sh
