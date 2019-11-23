27dff9384bc8da7cea71cd3d1231e66573aac01a
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-11-23 08:41:19 -0500

    ci: try yaml anchors to add back trusty

--- .ci/install.sh
+++ .ci/install.sh
@@ -12,10 +12,6 @@ nvm use lts/carbon
 
 echo node $(node --version)
 echo npm $(npm --version)
-python --version
-python -m pip --version
-python3 --version
-python3 -m pip --version
 ruby --version
 echo gem $(gem --version)
 
