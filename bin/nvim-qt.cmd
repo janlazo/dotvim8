ecf16aedecb59d031645f95608ff5713357001f9
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2020-01-26 22:20:15 -0500

    nvim-qt: fix PATHEXT before running the binary
    PATHEXT must have its default extensions to run Windows programs
    without their file extensions.
    Doing it inside Vim,Neovim risks running programs before fixing PATHEXT.

--- bin/nvim-qt.cmd
+++ bin/nvim-qt.cmd
@@ -1,3 +1,4 @@
 @echo off
-set QT_AUTO_SCREEN_SCALE_FACTOR=1
+set "PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC"
+set "QT_AUTO_SCREEN_SCALE_FACTOR=1"
 start "nvim-qt" /min cmd.exe /s /c "nvim-qt.exe -qwindowgeometry 800x600 %*"
