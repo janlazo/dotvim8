7671ffaa75943640906c11e710e423e261df8d88
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2020-11-03 19:29:26 -0500

    plug: install providers system-wide
    They do not work well when installed within user directory.
    Assume that language version managers (ie. pyenv, chruby)
    or local package managers (ie. scoop) are installed
    to avoid Python and Ruby broken behaviors, especially on Windows.

--- bin/install_deps.sh
+++ bin/install_deps.sh
@@ -4,31 +4,32 @@
 set -eu
 
 # Javascript (https://github.com/neovim/node-client)
-# Need 3.5.2+ on Windows
+# Assume that nvm or nvs are installed to avoid 'sudo'.
 if (command -v node && command -v npm) > /dev/null 2>&1; then
   npm install -g \
     neovim@4.5.0 dockerfile-language-server-nodejs
 fi
 
 # Python (https://github.com/neovim/python-client)
+# Assume that pyenv is installed to avoid 'sudo'.
 for py in python3 python; do
   if (command -v $py && $py -m pip --version) >/dev/null 2>&1; then
-    $py -m pip install --user \
+    $py -m pip install \
       pynvim flake8
     break
   fi
 done
 
 # Ruby (https://github.com/neovim/neovim-ruby)
-# Need 0.9.0+ for rubyeval()
+# Assume that chruby and ruby-install are installed to avoid 'sudo'.
 if (command -v ruby && command -v gem) > /dev/null 2>&1; then
-  GEM_INSTALL="gem install --user-install --conservative --minimal-deps --no-document --no-suggestions"
+  GEM_INSTALL="gem install --conservative --minimal-deps --no-document --no-suggestions"
   eval "$GEM_INSTALL solargraph"
   eval "$GEM_INSTALL --prerelease --version '> 0.8.1' neovim"
 fi
 
 # Perl (https://github.com/jacquesg/p5-Neovim-Ext)
-# Assume that perl uses local::lib.
+# Assume that perl uses local::lib to avoid 'sudo'.
 if (command -v perl && command -v cpanm) > /dev/null 2>&1; then
   cpanm -n Neovim::Ext
 fi
